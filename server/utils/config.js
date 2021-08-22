e998588d07d9fb55b91d5e7714f66899b8e4dfc6
Leevi <leevi.parkkinen@aalto.fi>
2021-08-22 05:15:42 +0300

    testing

--- server/utils/config.js
+++ server/utils/config.js
@@ -8,11 +8,16 @@ let PORT = process.env.PORT || 3003
 
 const mongouri = () => {
   if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'prodtesting') return process.env.TEST_MONGODB_URI
-  else if (process.env.NODE_ENV === 'pipelinetest') return process.argv[2]
+  else if (process.env.NODE_ENV === 'pipelinetest') {
+    const i = process.argv.length
+    return process.argv[i-1]
+  }
   else return process.env.MONGODB_URI
 }
 
 const MONGODB_URI = mongouri()
+
+console.log(MONGODB_URI)
 
 module.exports = {
   inProduction,
